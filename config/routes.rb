9cf960a0d4b07f81e436be7e137a349b9e4f0f7e
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-11-13 13:03:30 +0100

    Corrected doses#destroy; set up CSS grid for cocktails#index

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   get 'cocktails/:cocktail_id/doses/new', to: 'doses#new', as: :new_dose
   post 'cocktails/:cocktail_id/doses', to: 'doses#create', as: :cocktail_doses
-  delete 'doses/:id', to: 'doses#destroy'
+  delete 'doses/:id', to: 'doses#destroy', as: :cocktail_dose
 end
